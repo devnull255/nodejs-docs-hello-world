a84279c364eeef3f284ff6e51414cc7e0bdf5fe2
Michael Mabin <mabinm@comcast.net>
2019-03-24 09:41:14 -0500

    add jenkins + docker to response.end method call

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello Beautiful World!");
+    response.end("Hello Beautiful World from Jenkins + Docker!");
 });
 
 const port = process.env.PORT || 1337;
